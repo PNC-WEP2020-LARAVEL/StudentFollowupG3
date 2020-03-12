8b0487521990a300a80a9e8e5ce954ee30d3416b
sovonthy.leng <sovonthy.leng@PNC.LAN>
2020-03-12 09:17:46 +0700

    add form student

---
+++ database/migrations/2020_03_12_015326_create_students_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->bigIncrements('student_id');
+            $table->timestamps();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('gender');
+            $table->string('class');
+            $table->string('year');
+            $table->string('picture');
+            $table->string('province');
+            $table->unSignedBigInteger('user_id');
+            $table->foreign('user_id')
+            ->references('id')
+            ->on('users')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <a class="btn btn-info" href="{{route('posts.create')}}">Add Student</a>
+</div>
+@endsection
